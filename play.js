6f1fd2d38f5d696d6e5a6bc706165e558cb3e9c5
maggiezhao11 <maggiezhao06@hotmail.com>
2021-06-30 17:32:52 +0000

    connect to server and getting data from server

---
+++ play.js
@@ -0,0 +1,24 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.223.133', // IP address here,
+    port: '50542' // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on('data', function(data) {
+    console.log(data); 
+  });
+
+
+
+
+  
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
