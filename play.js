0c0dc8dfbb3764c1a37890ca303a7c14bb22e69b
maggiezhao11 <maggiezhao06@hotmail.com>
2021-06-30 19:21:49 +0000

    add message and user name once connection is successfully made

--- play.js
+++ play.js
@@ -6,19 +6,23 @@ const connect = function () {
     host: '135.23.223.133', // IP address here,
     port: '50542' // PORT number here,
   });
+  // show the connect information to user once the connection is successfully made. 
+  conn.on('connect', function() {
+    console.log ('Successfully connected to game server!')
+  //write/sending an userName to server;  
+  conn.write('Name: MZ');
+  });
+  
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on('data', function(data) {
     console.log(data); 
   });
-
-
-
-
-  
   return conn;
 };
 
 console.log("Connecting ...");
 connect();
+
+module.exports = connect;
