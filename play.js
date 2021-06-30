66e1fb7aac8abe665ed4d2010898d3b6ef04d8ae
maggiezhao11 <maggiezhao06@hotmail.com>
2021-06-30 22:54:22 +0000

    refactor

--- play.js
+++ play.js
@@ -1,44 +1,7 @@
-const net = require("net");
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133', // IP address here,
-    port: '50542' // PORT number here,
-  });
-    conn.on('connect', function() {
-    console.log ('Successfully connected to game server!')
-    conn.write('Name: MZ');
-    //const directions = ['up', 'right', 'right', 'down', 'left'];
-    //setTimeout(() => {conn.write('Move: up')}, 500);
-    //setTimeout(() => {conn.write('Move: left')}, 500);
-    //setInterval(() => {conn.write('Move: up')}, 1000);
-
-  });
-
-    conn.setEncoding("utf8");
-    conn.on('data', function(data) {
-      console.log(data); 
-    });
-    return conn;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-    }
-  };
-    stdin.on("data",(key) => handleUserInput(key));
-  return stdin;
-};
 setupInput();
-
-module.exports = connect;
-
