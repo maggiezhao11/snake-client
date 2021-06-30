6cd7d10c93a7361f3300f097d72e141d3fd0233b
maggiezhao11 <maggiezhao06@hotmail.com>
2021-06-30 22:35:05 +0000

    setting up stdin

--- play.js
+++ play.js
@@ -1,28 +1,44 @@
 const net = require("net");
-
-// establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: '135.23.223.133', // IP address here,
     port: '50542' // PORT number here,
   });
-  // show the connect information to user once the connection is successfully made. 
-  conn.on('connect', function() {
+    conn.on('connect', function() {
     console.log ('Successfully connected to game server!')
-  //write/sending an userName to server;  
-  conn.write('Name: MZ');
-  });
-  
+    conn.write('Name: MZ');
+    //const directions = ['up', 'right', 'right', 'down', 'left'];
+    //setTimeout(() => {conn.write('Move: up')}, 500);
+    //setTimeout(() => {conn.write('Move: left')}, 500);
+    //setInterval(() => {conn.write('Move: up')}, 1000);
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on('data', function(data) {
-    console.log(data); 
   });
-  return conn;
+
+    conn.setEncoding("utf8");
+    conn.on('data', function(data) {
+      console.log(data); 
+    });
+    return conn;
 };
 
 console.log("Connecting ...");
 connect();
 
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+    }
+  };
+    stdin.on("data",(key) => handleUserInput(key));
+  return stdin;
+};
+setupInput();
+
 module.exports = connect;
+
