6e1b46ee0168d93e70beafee4d1de4e2fb1e8a8b
maggiezhao11 <maggiezhao06@hotmail.com>
2021-06-30 22:54:46 +0000

    refactor

---
+++ input.js
@@ -0,0 +1,15 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+    }
+  };
+    stdin.on("data",(key) => handleUserInput(key));
+  return stdin;
+};
+
+module.exports = {setupInput};
