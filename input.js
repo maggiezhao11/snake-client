d8b89cd2fbd86b35258db2c7398ca64c12ff9d56
maggiezhao11 <maggiezhao06@hotmail.com>
2021-07-01 00:30:11 +0000

    add some message during game

--- input.js
+++ input.js
@@ -1,12 +1,35 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   const handleUserInput = function (key) {
+  if (key === 'w') {
+    conn.write('Move: up');
+  }
+  if (key === 'a') {
+    conn.write('Move: left');
+  }
+  if (key === 's') {
+    conn.write('Move: down');
+  }
+  if (key === 'd') {
+    conn.write('Move: right');
+  }
   if (key === '\u0003') {
     process.exit();
     }
+    if (key === 'y') {
+      conn.write('Say: Hyo');
+      }
+    if (key === 'l') {
+      conn.write('Say: LOL');
+      }
+
+
   };
     stdin.on("data",(key) => handleUserInput(key));
   return stdin;
